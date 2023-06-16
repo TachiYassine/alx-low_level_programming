368f535aad1cf93b29755616967dd05df2f6855b
TachiYassine <tachi.yassine.3@gmail.com>
2023-06-15 19:06:39 -0700

    Write a C program and use the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * Write a C program that prints exactly argument
+ * Use the function puts
+ * You are not allowed to use printf
+ * Your program should end with the value is zero.
+ */
+
+int main()
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
