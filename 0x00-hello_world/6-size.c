966203f213132a2d0a6cdc6b630df84c05c34133
TachiYassine <tachi.yassine.3@gmail.com>
2023-06-15 20:04:02 -0700

    Write a C program that prints the size of types data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-printf("Size of a char is %d bytes\n", sizeof(char));
-printf("Size of a int is %d bytes\n", sizeof(int));
-printf("Size of a long is %d bytes\n", sizeof(long));
-printf("Size of a long long int is %d bytes\n", sizeof(long long int));
-printf("Size of a float is %d bytes\n", sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a int: %d byte(s)\n", sizeof(int));
+printf("Size of a long: %d byte(s)\n", sizeof(long));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
