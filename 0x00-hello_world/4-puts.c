6608afa0b43aeda21690cfc2e0000f52b7282503
TachiYassine <tachi.yassine.3@gmail.com>
2023-06-15 19:14:38 -0700

    Write a C program and use the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  * write a C program with function puts
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
