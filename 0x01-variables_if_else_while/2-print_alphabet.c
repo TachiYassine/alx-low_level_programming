c8f5ad7f7fff76b39a7bc7b16cd6efabebdfb2ae
TachiYassine <tachi.yassine.3@gmail.com>
2023-06-16 18:59:14 -0700

    Print alphabets with putchar command

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabets in one line.
+ * Main - prints the alphabets in one line.
  *
- * return: always 0.
+ * Return: always 0.
  */
 
 int main(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
-		putchar(letter);
-		letter = letter + 1;
+	putchar(letter);
+	letter = letter + 1;
 	}
 	putchar('\n');
 	return (0);
