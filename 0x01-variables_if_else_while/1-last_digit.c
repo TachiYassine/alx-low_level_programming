01fe3112fe47c18f935427ce377fbce9542be699
TachiYassine <tachi.yassine.3@gmail.com>
2023-06-16 18:36:28 -0700

    The last digit with if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,8 @@
 
 /**
  * main - This program will assign a random number in variable n
- *        And print the last digit from this number
- *        whether it is greater than 5 or less than 6 or equal 0.
+ *	And print the last digit from this number
+ *	whether it is greater than 5 or less than 6 or equal 0.
  *
  * Return: always 0.
  */
@@ -13,22 +13,22 @@
 int main(void)
 {
 	int n;
-	int last_digit;
+	int digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10 ;
-	if (last_digit > 5)
+	digit = n % 10;
+	if (digit > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n , last_digit);
+	printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
-	else if (last_digit == 0)
+	else if (digit == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n , last_digit);
+	printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
-	else if (last_digit < 6 && last_digit != 0)
-        {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n , last_digit);
-        }
+	else if (digit < 6 && digit != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	}
 	return (0);
 }
