f47c241ca5b3089c04b520f7f658c90e5d664f98
TachiYassine <tachi.yassine.3@gmail.com>
2023-06-21 18:27:39 -0700

    Prototypes & functions & header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,14 @@
  * Return: 0
  */
 
-int main(void){
-
+int main(void)
+{
 	int i;
 	char word[] = "_putchar";
 
-	for (i = 0; i < 8; i++){
-	      	_putchar(word[i]);
-	}
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
 	_putchar('\n');
 	
-	return 0;
+	return (0);
 }
