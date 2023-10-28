b977b8617be3bc3a6bb2599d26035f419f55a350
TachiYassine <tachi.yassine.3@gmail.com>
2023-10-27 20:06:23 -0700

    done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,12 +7,15 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
+
 	int i;
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
+
 	return (0);
+
 }
